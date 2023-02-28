d8bae9592f0c2f0e6ed24b68dc1ec9f26811aa7b
YuliiaMoholivets <112686109+YuliiaMoholivets@users.noreply.github.com>
2023-02-28 21:19:38 +0000

    Deploying to gh-pages from @ YuliiaMoholivets/goit-react-hw-08-phonebook@a8f49643708af41ae07b7e3138d9d21e40869a33 🚀

---
+++ static/js/575.8eed303f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[575],{9575:function(e,t,n){n.r(t);var r=n(2791),u=n(7689),c=n(3329);t.default=function(){var e=(0,u.s0)();return(0,r.useEffect)((function(){var t=setTimeout((function(){e("/")}),5e3);return function(){return clearTimeout(t)}}),[e]),(0,c.jsx)("div",{children:(0,c.jsx)("h3",{children:"Page not Found!"})})}}}]);
+//# sourceMappingURL=575.8eed303f.chunk.js.map
